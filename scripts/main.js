5a36fb8f467cf810409434454d0b8221e922a317
marcosv38 <50200539+marcosv38@users.noreply.github.com>
2024-08-30 22:31:14 -0300

    insere funcionalidades de download de imagem e ajuste no apagamento das imagens

--- scripts/main.js
+++ scripts/main.js
@@ -17,7 +17,7 @@ orientacaoHorizontal.addEventListener('click', (evento) => {
         orientacaoAtual = "h";
 
         trocaButoesModelo();
-        if(containerMosaico.className!=="")
+        if (containerMosaico.className !== "")
             atualizaLayoutModel();
     }
 });
@@ -31,7 +31,7 @@ orientacaoVertical.addEventListener('click', (evento) => {
         orientacaoAtual = "v";
 
         trocaButoesModelo();
-        if(containerMosaico.className!=="")
+        if (containerMosaico.className !== "")
             atualizaLayoutModel();
     }
 });
@@ -41,7 +41,7 @@ botoesLayout.forEach(botao => {
     botao.addEventListener('click', (evento) => {
         qtdFtsMosaico = evento.target.id;
         const modeloAtivo = document.querySelector(".activeModel");
-        if(modeloAtivo!==null)
+        if (modeloAtivo !== null)
             modeloAtivo.classList.remove("activeModel");
         evento.target.classList.add("activeModel");
         atualizaLayoutModel();
@@ -51,9 +51,9 @@ botoesLayout.forEach(botao => {
 
 
 function atualizaLayoutModel() {
-    if(containerMosaico.className!==""){
+    if (containerMosaico.className !== "") {
         containerMosaico.classList.remove(containerMosaico.className);
-        
+
     }
     containerMosaico.classList.add(`imagem__mosaico-moldura${qtdFtsMosaico}${orientacaoAtual}`);
     containerMosaico.innerHTML = "";
@@ -131,18 +131,18 @@ let ajustesImg = {
     zoom: 50
 }
 
-slidersDeAjuste[0].addEventListener('input',(evento)=>{
+slidersDeAjuste[0].addEventListener('input', (evento) => {
     ajustesImg.posicaoVertical = evento.target.value;
     atualizarPosicionamentoImg();
 });
 
 
-slidersDeAjuste[1].addEventListener('input',(evento)=>{
+slidersDeAjuste[1].addEventListener('input', (evento) => {
     ajustesImg.posicaoHorizontal = evento.target.value;
     atualizarPosicionamentoImg();
 });
 
-slidersDeAjuste[2].addEventListener('input',(evento)=>{
+slidersDeAjuste[2].addEventListener('input', (evento) => {
     ajustesImg.zoom = evento.target.value;
     atualizarPosicionamentoImg();
 });
@@ -152,14 +152,14 @@ function ativarClickImg() {
     imagensMosaico.forEach((imagem) => {
         imagem.addEventListener('click', (item) => {
             imagemSelecionada = item.target.id;
-            console.log(imagemSelecionada);
+            inserirImgMosaico(item.target);
         });
     });
 }
 
-function atualizarPosicionamentoImg(){
+function atualizarPosicionamentoImg() {
     const imgSelecionada = document.getElementById(imagemSelecionada);
-    imgSelecionada.setAttribute("style",`object-position: ${ajustesImg.posicaoHorizontal}% ${ajustesImg.posicaoVertical}%; zoom:${ajustesImg.zoom}%`);
+    imgSelecionada.setAttribute("style", `object-position: ${ajustesImg.posicaoHorizontal}% ${ajustesImg.posicaoVertical}%; zoom:${ajustesImg.zoom}%`);
 }
 
 
@@ -167,13 +167,12 @@ function atualizarPosicionamentoImg(){
 const botaoCarregarImagem = document.querySelector('.container-imagem__upload__button');
 const inputUpload = document.getElementById("image-upload");
 const containerNomeImg = document.querySelector(".container-imagem__upload__itens-lista");
+const botaoApagarImagens = document.querySelector(".container-imagem__button__apagar");
+let listaImagens = document.querySelectorAll('.container-imagem__upload__itens-item');
+let imagemSelecionadaDaLista = "";
 
 let imagensCarregadas = [];
-let imagemCarregadaObj = {
-    src: "",
-    nome: "",
-    tipo:""
-}
+
 
 
 botaoCarregarImagem.addEventListener("click", () => {
@@ -185,7 +184,7 @@ function lerConteudoDoArquivo(arquivo) {
     return new Promise((resolve, reject) => {
         const leitor = new FileReader();
         leitor.onload = () => {
-            resolve({ url: leitor.result, nome: arquivo.name , tipo: arquivo.type})
+            resolve({ url: leitor.result, nome: arquivo.name, tipo: arquivo.type })
         }
 
         leitor.onerror = () => {
@@ -199,7 +198,11 @@ function lerConteudoDoArquivo(arquivo) {
 
 inputUpload.addEventListener("change", async (evento) => {
     const arquivo = evento.target.files[0];
-    
+    let imagemCarregadaObj = {
+        src: "",
+        nome: "",
+        tipo: ""
+    }
     if (arquivo) {
         try {
             const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
@@ -207,16 +210,124 @@ inputUpload.addEventListener("change", async (evento) => {
             imagemCarregadaObj.nome = conteudoDoArquivo.nome;
             imagemCarregadaObj.tipo = conteudoDoArquivo.tipo;
             imagensCarregadas.push(imagemCarregadaObj);
-            containerNomeImg.innerHTML+=`
-                <li class="container-imagem__upload__itens-item" id="imgList${imagemCarregadaObj.length-1}">
+            
+            containerNomeImg.innerHTML += `
+                <li class="container-imagem__upload__itens-item" id="imgList-${imagensCarregadas.length - 1}">
                     <p class="container-imagem__upload__itens-item__nome">${imagemCarregadaObj.nome}</p>
-                    <img src="img/lixeira.png" alt="icone de apagar imagem"
-                    class="container-imagem__upload-button-icon">
+                    <button class="container-imagem__upload-button-icon bt-lixeira" id="apagarImg-${imagensCarregadas.length - 1}"><img src="img/lixeira.png" alt="icone de apagar imagem"
+                    class="container-imagem__upload-button-icon" ></button>
                 </li>
             `;
+            listaImagens = document.querySelectorAll('.container-imagem__upload__itens-item');
+
+            selecionarImagemDaLista();
+            console.log(imagensCarregadas);
         } catch (erro) {
             console.error(erro)
         }
     }
 })
+
+
+botaoApagarImagens.addEventListener("click", () => {
+    if (confirm("Você quer apagar todas as imagens carregadas?")) {
+        imagensCarregadas = [];
+        listaImagens = [];
+        containerNomeImg.innerHTML = '';
+        const imagensMosaico = document.querySelectorAll(".imagem__mosaico-base");
+        imagensMosaico.forEach((img)=>{
+            img.setAttribute('src',"/img/imgBase.png");
+        });
+    }
+});
+
+function selecionarImagemDaLista() {
+    listaImagens.forEach((item) => {
+        item.addEventListener('click', (evento) => {
+            const imgSelecinada = document.querySelectorAll(".container-imagem__upload__itens-item-selecionada");
+            imgSelecinada.forEach((elemento) => {
+                if (elemento.classList.contains("container-imagem__upload__itens-item-selecionada"))
+                    elemento.classList.remove("container-imagem__upload__itens-item-selecionada");
+            });
+
+            if (imagemSelecionadaDaLista !== evento.target.closest("li").id) {
+                imagemSelecionadaDaLista = evento.target.closest("li").id;
+                evento.target.closest("li").classList.add('container-imagem__upload__itens-item-selecionada');
+                console.log(imagemSelecionadaDaLista.split("-")[1]);
+            } else {
+                imagemSelecionadaDaLista = "";
+            }
+        });
+    });
+}
+function inserirApagarImg(){
+    const btApagarImg = document.querySelectorAll(".bt-lixeira");
+    btApagarImg.forEach((lixeira)=>{
+        btApagarImg.addEventListener((evento)=>{
+            alert("id selecionado="+evento.target.id.split("-")[1]);
+        });
+    });
+}
+
+
+
+//---------------- inserindo imagens no mosaico ----------------
+
+
+function inserirImgMosaico(img) {
+    if (imagemSelecionadaDaLista !== "") {
+        img.setAttribute("src", imagensCarregadas[imagemSelecionadaDaLista.split("-")[1]].src);
+    }
+}
+
+//------------------- Fazendo o download do mosaico ---------------------
+let extensaoArquivo = "PNG";
+const nomeArquivoInput = document.querySelector(".container-opcoes__download__nome-arquivo");
+const botaoDownload = document.querySelector(".container-opcoes__download__botao-download");
+const botoesExtensaoJpeg = document.querySelector(".container-opcoes__download__extensao__botao-Jpeg");
+const botoesExtensaoPNG = document.querySelector(".container-opcoes__download__extensao__botao-PNG");
+const botoesExtensaoWebp = document.querySelector(".container-opcoes__download__extensao__botao-Webp");
+
+botoesExtensaoJpeg.addEventListener('click',(bt)=>{
+    extensaoArquivo = "Jpeg";
+    limparEstiloActive("bt__menu__Download-Selecionado", bt);
+
+});
+
+botoesExtensaoPNG.addEventListener('click',(bt)=>{
+    extensaoArquivo = "PNG";
+    limparEstiloActive("bt__menu__Download-Selecionado",bt);
+});
+
+botoesExtensaoWebp.addEventListener('click',(bt)=>{
+    extensaoArquivo = "Webp";
+    limparEstiloActive("bt__menu__Download-Selecionado",bt);
+
+});
+
+function limparEstiloActive(classe,addClassAtivo){
+    const ativos = document.querySelectorAll("."+classe);
+    ativos.forEach((bt)=>{
+        bt.classList.remove(classe);
+    });
+    addClassAtivo.target.classList.add(classe);
+}
+
+
+botaoDownload.addEventListener('click', () => {
+    const mosaico = document.querySelector('.container__moisaico');
+
+    html2canvas(document.querySelector("#container__moisaico")).then((canvas) => {
+        document.querySelector(".imagem__mosaico-bg").appendChild(canvas);
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL(`image/${extensaoArquivo.toLowerCase()}`);
+        link.download = `${nomeArquivoInput.value}.${extensaoArquivo.toLowerCase()}`;
+        link.click();
+        canvas.remove();
+        alert("Esse projeto foi criado como forma de pesquisa. Infelizmente a Html2canvas ainda não possui suporte para as tags css utilizadas no mosaico e a imagem baixada não condiz perfeitamente com o proposto.")
+    });
+
+});
+
+
 
